120beeab79e32950f0c3f477b4ffaf2094ec32a0
pengxing <pengxing@maihaoche.com>
2021-03-17 16:04:17 +0800

    作业三，扩展本地配置文件

---
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/configuration/microprofile/config/source/ApplicationConfigSource.java
@@ -0,0 +1,63 @@
+package org.geektimes.configuration.microprofile.config.source;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * ApplicationConfigSource
+ *
+ * @author liuhao
+ */
+public class ApplicationConfigSource implements ConfigSource {
+
+    /**
+     * 应用属性
+     */
+    private final Map<String, String> properties;
+
+    public ApplicationConfigSource() {
+        Map applicationProperties = init();
+        this.properties = new HashMap<>(applicationProperties);
+    }
+
+    public Map<String, String> init() {
+        Map<String, String> result = new HashMap<>();
+        try {
+            InputStream inputStream = ApplicationConfigSource.class.getResourceAsStream("/META-INF/application.properties");
+            byte[] array = new byte[128];
+            int byteNumber = inputStream.read(array);
+            String context = new String(array, 0, byteNumber, "UTF-8");
+            String[] lines = context.split("\n");
+            for (int i = 0; i < lines.length; i++) {
+                String line = lines[i];
+                if (!line.contains("=")) {
+                    continue;
+                }
+                String[] lineArray = line.split("=");
+                result.put(lineArray[0], lineArray[1]);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return properties.keySet();
+    }
+
+    @Override
+    public String getValue(String propertyName) {
+        return properties.get(propertyName);
+    }
+
+    @Override
+    public String getName() {
+        return "Application Config";
+    }
+}
--- projects/stage-0/user-platform/user-web/src/test/java/org/geektimes/configuration/demo/PreferencesDemo.java
+++ projects/stage-0/user-platform/user-web/src/test/java/org/geektimes/configuration/demo/PreferencesDemo.java
@@ -1,15 +1,25 @@
 package org.geektimes.configuration.demo;
 
-import java.util.prefs.BackingStoreException;
+import org.geektimes.configuration.microprofile.config.JavaConfig;
+
+import java.util.Optional;
 import java.util.prefs.Preferences;
 
 public class PreferencesDemo {
 
-    public static void main(String[] args) throws BackingStoreException {
+    public static void main(String[] args) throws Exception {
         Preferences userPreferences = Preferences.userRoot();
         userPreferences.put("my-key", "Hello,World");
         userPreferences.flush();
-        System.out.println(userPreferences.get("my-key", null));
-//        Preferences.systemRoot();
+        JavaConfig javaConfig = new JavaConfig();
+        Optional<Long> optionalValue1 = javaConfig.getOptionalValue("sun.arch.data.model", Long.class);
+        Optional<Double> optionalValue2 = javaConfig.getOptionalValue("java.specification.version", Double.class);
+        System.out.println(optionalValue1.get());
+        System.out.println(optionalValue2.get());
+
+        Optional<String> optionalValue3 = javaConfig.getOptionalValue("application.name", String.class);
+        Optional<Double> optionalValue4 = javaConfig.getOptionalValue("application.version", Double.class);
+        System.out.println(optionalValue3.get());
+        System.out.println(optionalValue4.get());
     }
 }
