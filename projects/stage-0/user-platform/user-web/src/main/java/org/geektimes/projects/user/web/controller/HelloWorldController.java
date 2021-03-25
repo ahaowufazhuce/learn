4cc17389ef66010905624bbbb002aea9f785f57b
pengxing <pengxing@maihaoche.com>
2021-03-25 08:41:47 +0800

    作业四，代码整理

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/HelloWorldController.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/HelloWorldController.java
@@ -5,7 +5,6 @@ import org.geektimes.web.mvc.controller.PageController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 /**
@@ -16,7 +15,6 @@ public class HelloWorldController implements PageController {
 
     @Override
     @GET
-    @POST
     @Path("/world") // /hello/world -> HelloWorldController
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
         return "index.jsp";
--- projects/stage-0/user-platform/user-web/src/test/java/org/geektimes/configuration/demo/PreferencesDemo.java
+++ projects/stage-0/user-platform/user-web/src/test/java/org/geektimes/configuration/demo/PreferencesDemo.java
@@ -1,30 +1,25 @@
 package org.geektimes.configuration.demo;
 
-import org.geektimes.configuration.microprofile.config.JavaConfig;
-
-import java.util.Optional;
-import java.util.prefs.Preferences;
-
 public class PreferencesDemo {
 
-    public static void main(String[] args) throws Exception {
-        Preferences userPreferences = Preferences.userRoot();
-        userPreferences.put("my-key", "Hello,World");
-        userPreferences.flush();
-        JavaConfig javaConfig = new JavaConfig();
-        Optional<Long> optionalValue1 = javaConfig.getOptionalValue("sun.arch.data.model", Long.class);
-        Optional<Double> optionalValue2 = javaConfig.getOptionalValue("java.specification.version", Double.class);
-        System.out.println(optionalValue1.get());
-        System.out.println(optionalValue2.get());
-
-        Optional<String> optionalValue3 = javaConfig.getOptionalValue("application.name", String.class);
-        Optional<Double> optionalValue4 = javaConfig.getOptionalValue("application.version", Double.class);
-        System.out.println(optionalValue3.get());
-        System.out.println(optionalValue4.get());
-
-        Optional<String> optionalValue5 = javaConfig.getOptionalValue("PWD", String.class);
-        Optional<Double> optionalValue6 = javaConfig.getOptionalValue("VERSIONER_PYTHON_VERSION", Double.class);
-        System.out.println(optionalValue5.get());
-        System.out.println(optionalValue6.get());
-    }
+//    public static void main(String[] args) throws Exception {
+//        Preferences userPreferences = Preferences.userRoot();
+//        userPreferences.put("my-key", "Hello,World");
+//        userPreferences.flush();
+//        JavaConfig javaConfig = new JavaConfig();
+//        Optional<Long> optionalValue1 = javaConfig.getOptionalValue("sun.arch.data.model", Long.class);
+//        Optional<Double> optionalValue2 = javaConfig.getOptionalValue("java.specification.version", Double.class);
+//        System.out.println(optionalValue1.get());
+//        System.out.println(optionalValue2.get());
+//
+//        Optional<String> optionalValue3 = javaConfig.getOptionalValue("application.name", String.class);
+//        Optional<Double> optionalValue4 = javaConfig.getOptionalValue("application.version", Double.class);
+//        System.out.println(optionalValue3.get());
+//        System.out.println(optionalValue4.get());
+//
+//        Optional<String> optionalValue5 = javaConfig.getOptionalValue("PWD", String.class);
+//        Optional<Double> optionalValue6 = javaConfig.getOptionalValue("VERSIONER_PYTHON_VERSION", Double.class);
+//        System.out.println(optionalValue5.get());
+//        System.out.println(optionalValue6.get());
+//    }
 }
